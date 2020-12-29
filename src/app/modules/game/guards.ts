60577e1f1647d1cab24cfdd695ce244a0a843d73
Mikolaj Serwicki <mikolaj.serwicki@skygate.io>
2020-12-29 04:52:43 +0100

    feat(game): add guards

---
+++ src/app/modules/game/guards.ts
@@ -0,0 +1,8 @@
+import { ApiResource, People, StarShip } from './models';
+import { Game, GameMode, MultiPlayerGame, SinglePlayerGame } from './types';
+
+export const isSinglePlayerGame = (game: Game): game is SinglePlayerGame => game.mode === GameMode.SINGLE_PLAYER;
+export const isMultiPlayerGame = (game: Game): game is MultiPlayerGame => game.mode === GameMode.MULTI_PLAYER;
+
+export const isStartShipResource = (resource: ApiResource): resource is StarShip => resource.hasOwnProperty('crew');
+export const isPeopleResource = (resource: ApiResource): resource is People => resource.hasOwnProperty('mass');
