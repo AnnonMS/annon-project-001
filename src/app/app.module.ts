1767ce36ef49c7c173ac0ee59523a7372c5642a8
Mikolaj Serwicki <mikolaj.serwicki@skygate.io>
2020-12-29 04:42:43 +0100

    test(mocks): add swapi mocks data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,13 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
+import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgxsStoreModule } from '@store/store.module';
+import { AppComponent } from './app.component';
+import { GameModule } from './modules';
 
 @NgModule({
     declarations: [AppComponent],
-    imports: [BrowserModule, BrowserAnimationsModule],
-    providers: [],
+    imports: [BrowserModule, NgxsStoreModule, GameModule, BrowserAnimationsModule],
     bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/modules/game/game.module.ts
+++ src/app/modules/game/game.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MaterialModule } from '@app/material/material.module';
 import { SharedModule } from '@app/shared/shared.module';
+import { CardModule } from '../card/card.module';
+import { PlayerModule } from '../player/player.module';
 import { CreateGameFormComponent } from './components/create-game-form/create-game-form.component';
 import { ErrorComponent } from './components/error/error.component';
 import { GameBoardComponent } from './components/game-board/game-board.component';
@@ -10,7 +12,7 @@ import { GameComponent } from './containers/game/game.component';
 
 @NgModule({
     declarations: [GameBoardComponent, CreateGameFormComponent, GameComponent, ErrorComponent],
-    imports: [CommonModule, MaterialModule, ReactiveFormsModule, SharedModule],
+    imports: [CommonModule, MaterialModule, CardModule, ReactiveFormsModule, PlayerModule, SharedModule],
     exports: [GameComponent],
 })
 export class GameModule {}
